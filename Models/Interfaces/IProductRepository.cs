c843cdf5b1c65eae9cbafd2ee24fadf300b8ba3f
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-07-24 13:32:33 +0100

    Interface repo

---
+++ Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+
+        Task<Product> GetProductByNameAsync(string name);
+
+    }
+}
